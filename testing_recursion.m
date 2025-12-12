c36be109541a5d8df0c0cb9f05eb2ebc6f4b9390
Dulanjana Perera <dulanjaperera@gmail.com>
2025-12-12 11:54:14 -0600

    Successful testing. Both symbolic and the conventional methods yield the same output.

--- testing_recursion.m
+++ testing_recursion.m
@@ -1,8 +1,22 @@
 clear
 N = 2;
 syms 'l' [N 3] 
-syms T(l)
+% syms T(l)
+T = eye(4); 
 for n=1:N 
-    T_n = HTM_sym([0; l(n,2); l(n,3)], 1, 0.278, 0.013);
+    [T_n, ~, ~] = HTM_sym([0, l(n,2), l(n,3)], 1, 0.278, 0.013);
     T = T * T_n;
 end
+
+l_num = zeros(N,3);   % initialize
+l_num(1,2) = 0.01;
+l_num(1,3) = 0.01;
+l_num(2,2) = 0.01;
+l_num(2,3) = 0.01;
+
+% Evaluate numerically
+T_num = double(subs(T, l, l_num));
+
+TT=HTM(l_num(1,:))*HTM(l_num(2,:));
+
+eT = T_num-TT;
