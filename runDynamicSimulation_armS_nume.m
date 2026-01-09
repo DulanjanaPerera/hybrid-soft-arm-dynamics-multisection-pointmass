23556adb454ed6d936bee7db7e3bceecb4d351b1
Dulanjana Perera <dulanjaperera@gmail.com>
2026-01-09 12:55:54 -0600

    parameter change

--- runDynamicSimulation_armS_nume.m
+++ runDynamicSimulation_armS_nume.m
@@ -1,6 +1,6 @@
 clear
 N = 3;
-loc = 0.4;
+loc = 0.5;
 m = 0.1;
 stiff = 2.2e1;
 damp = 10;
@@ -35,8 +35,7 @@ tspan = [0 10];
 % Stiffer ODE solver is used as the matrices are tend to go ill-condition
 opts = odeset('RelTol',1e-6,'AbsTol',1e-8,'MaxStep',1e-3);
 tic
-[t, X] = ode15s(@(t,X) armS_dynamics_nume(t,X,params), tspan, X0, opts);
-% [t, X] = ode15s(@(t,X) armS_dynamics_recursive(t,X,params), tspan, X0, opts);
+[t, X] = ode15s(@(t,X) armS_dynamics_nume_N3(t,X,params), tspan, X0, opts);
 times = toc
 
 %% ===================== DRAW + PLOT STATES (SIMULTANEOUS ANIMATION) =====================
