bf7b9d9520d1db11cfc679c46e3a06b999267edb
Dulanjana Perera <dulanjaperera@gmail.com>
2026-01-27 00:12:33 -0600

    minor updates: move animation toa funciton

--- runDynamicSimulation_armS_nume.m
+++ runDynamicSimulation_armS_nume.m
@@ -1,6 +1,6 @@
 clear
-N = 2;
-loc = 0.5;
+N = 3;
+loc = 0.1;
 m = 0.1;
 A = pi*(0.013/2)^2;
 stiff = 2.2e3;
@@ -18,7 +18,7 @@ params.mi = m*ones(N,1);
 params.g = [0; 0; -9.81];
 params.K = stiff * eye(2*N);
 % params.tau = zeros(2*N,1);
-params.tau = -(A * 3 * 1e5)*ones(2*N,1);
+params.tau = -(A * 0.0 * 1e5)*ones(2*N,1);
 params.D = damp * eye(2*N);
 params.lKbounds = [-0.02; 0.02; 1e6]; % lmin, lmax, Kmax
 params.mu = 2000;
@@ -32,23 +32,23 @@ params.mu = 2000;
 %     ];
 
 l0 = -0.000* ones(N,2);
-l0(1,:) = -0.0;
-% l0(2,:) = -0.01;
+l0(1,:) = -0.01;
+l0(2,:) = -0.01;
 fl0 = reshape(l0',[2*N,1]); % flattern the matrix
 dl0 = 0.000001 * ones(2*N,1); % initial length change speed flatterned
 X0 = [fl0; dl0];
 
 % Integrate
-tspan = [0 5];
+tspan = [0 10];
 
 % Stiffer ODE solver is used as the matrices are tend to go ill-condition
 opts = odeset('RelTol',1e-8,'AbsTol',1e-10,'MaxStep',1e-2);
 tic
 % [t, X] = ode15s(@(t,X) armS_dynamics_nume(t,X,params), tspan, X0, opts);
-[t, X] = ode15s(@(t,X) armS_dynamics_N3_entry_mex(t, X, ...
-    params.L, params.r, params.cog_xi, params.mi, params.g, params.K, params.D, params.tau, params.mu, params.lKbounds), tspan, X0, opts);
-% [t, X] = ode15s(@(t,X) armS_dynamics_N3_entry_mex_mex(t, X, ...
+% [t, X] = ode15s(@(t,X) armS_dynamics_N3_entry_mex(t, X, ...
 %     params.L, params.r, params.cog_xi, params.mi, params.g, params.K, params.D, params.tau, params.mu, params.lKbounds), tspan, X0, opts);
+[t, X] = ode15s(@(t,X) armS_dynamics_N3_entry_mex_mex(t, X, ...
+    params.L, params.r, params.cog_xi, params.mi, params.g, params.K, params.D, params.tau, params.mu, params.lKbounds), tspan, X0, opts);
 times = toc
 
 %% ===================== DRAW + PLOT STATES (SIMULTANEOUS ANIMATION) =====================
